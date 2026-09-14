9f5c4cab12d6c062aa08d44b9ddea9e4384f08d4
manish-mobio <manish.patadiya@mobiosolutions.com>
2026-09-14 20:53:20 +0530

    fic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello from Nodes manish!\n');
+  response.end('Hello from Nodes,js!\n');
 });
 
 server.listen(port, () => {
